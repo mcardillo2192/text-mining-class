f3aed7b9d1345d23b8e40fc45f62e141ae1ee2e3
mcardillo2192 <72587119+mcardillo2192@users.noreply.github.com>
2022-09-29 17:34:13 -1000

    Add files via upload

---
+++ config.py
@@ -0,0 +1,11 @@
+#--------------------------------------------------------------------------------
+# These tokens are needed for user authentication.
+
+
+GdevKey = "AIzaSyD9CQepKbNVw7CXX5rggW6sC2W28sjAmQY" #replace with your Google API key
+patdevcx = '016938528964600140909:i6wj7bxhnra'  #custom search engine for patents
+Gdevcx = '016938528964600140909:wupywukfgda'  #general google search engine
+GSdevcx = '016938528964600140909:agrqxfwuuuq'  #google scholar
+GLIdevcx = '016938528964600140909:m1qf0rkoow4'  #google linked-in
+GPRdevcx = '016938528964600140909:zkjzldc4wwa'   #press releases
+GVCdevcx = '016938528964600140909:zkjzldc4wwa'   #venture capital
